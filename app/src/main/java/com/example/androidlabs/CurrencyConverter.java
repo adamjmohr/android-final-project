0ffb66611f505045f6c88447506671c59623535d
Adam Mohr <adamjmohr@gmail.com>
2019-11-23 22:30:57 -0500

    Currency Listview done.

--- app/src/main/java/com/example/androidlabs/CurrencyConverter.java
+++ app/src/main/java/com/example/androidlabs/CurrencyConverter.java
@@ -1,15 +1,33 @@
 package com.example.androidlabs;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CurrencyConverter extends AppCompatActivity {
+
+    private ArrayAdapter<String> theAdapter;
+    private ArrayList<String> currencies = new ArrayList<>(Arrays.asList("CAD", "HKD", "ISK", "PHP",
+            "DKK", "HUF", "CZK", "AUD", "RON", "SEK", "IDR", "INR", "BRL", "RUB", "HRK", "JPY", "THB", "CHF",
+            "SGD", "PLN", "BGN", "TRY", "CNY", "NOK", "NZD", "ZAR", "USD", "MXN", "ILS", "GBP", "KRW",
+            "MYR", "EUR"));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+        setContentView(R.layout.currency_layout);
+
+        ListView theList = findViewById(R.id.theList);
+        theAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, currencies);
+        theList.setAdapter(theAdapter);
+        theList.setOnItemClickListener((list, item, position, id) -> {
+
+        });
 
     }
 }
